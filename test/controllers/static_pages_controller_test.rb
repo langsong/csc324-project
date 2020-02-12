f3a01e5aa60e8f4b913fcc204d285b8251311980
langsong <songlang@grinnell.edu>
2020-02-12 11:38:22 -0600

    Small changes colors and title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,25 +6,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Music for you"
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App | Help"
+    assert_select "title", "Music for you | Help"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App | About"
+    assert_select "title", "Music for you | About"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App | Contact"
+    assert_select "title", "Music for you | Contact"
   end
 
 end
